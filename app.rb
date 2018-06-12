daca91e1d67adf304ef4efb44662d06493fa324a
keylimotino <stephanie48758@gmail.com>
2018-06-12 19:41:47 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -20,8 +20,8 @@ class App < Sinatra::Base
   end
 
   get "/multiply/:num1/:num2" do
-    @num1 = params[:]
-    @num2
+    @num1 = params[:36]
+    @num2 = params[:36]
     product
     return "product"
   end
